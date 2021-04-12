1e824de79a5d66e69bf3c24a77a5b1ea68b46643
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2021-04-12 21:00:07 +0800

    Feat: Header Style & Content Update

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,14 +1,31 @@
-import Link from '@/components/common/GatsbyLink';
 import React from 'react';
+import Link from '@/components/common/GatsbyLink';
+import { styled } from 'gatsby-theme-stitches/src/stitches.config';
 import DarkModeToggle from './DarkModeToggle';
+import SiteLogo from '~/static/favicon.svg';
+
+const Wrapper = styled('nav', {
+  height: '100px',
+  display: 'flex',
+  gap: '$4',
+  placeContent: 'center center',
+  alignItems: 'center',
+});
+
+const Logo = styled('img', {
+  width: '40px',
+  height: '40px',
+});
 
 const Header = () => {
   return (
-    <div>
+    <Wrapper>
       <Link to="/">Home</Link>
-      <p>Test</p>
+      <Link to="/tags/color">Color</Link>
+      <Logo src={SiteLogo} alt="Site Logo" />
+      <Link to="/tags/typography">Typography</Link>
       <DarkModeToggle />
-    </div>
+    </Wrapper>
   );
 };
 

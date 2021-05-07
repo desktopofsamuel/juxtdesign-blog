74adc774236e19e6ae0c9731e9e284c49f787119
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2021-05-07 23:52:43 +0800

    Add NO-CORS for Form

--- src/components/common/FooterForm.jsx
+++ src/components/common/FooterForm.jsx
@@ -1,47 +1,69 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { Formik, Field, Form } from 'formik';
 import fetch from 'node-fetch';
 
-const FooterForm = () => {
+const FooterForm = () => (
   // Pass the useFormik() hook initial form values and a submit function that will
 
   // be called when the form is submitted
 
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-    },
+  // fetch(
+  //   'https://emailoctopus.com/api/1.5/lists/50c6d144-ae86-11eb-a3d0-06b4694bee2a/contacts',
+  //   {
+  //     method: 'post',
+  //     body: JSON.stringify(data),
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   },
+  // );
 
-    onSubmit: (values) => {
-      const body = {
+  // const formik = useFormik({
+  //   initialValues: {
+  //     email: '',
+  //   },
+
+  //   onSubmit{ async (values) => {
+
+  //   },
+  // });
+
+  <Formik
+    initialValues={{
+      email: '',
+    }}
+    onSubmit={async (values) => {
+      const data = {
         api_key: '69dbb0df-2884-4aa8-aa8b-801c483963dc',
         email_address: values.email,
       };
+
       fetch(
         'https://emailoctopus.com/api/1.5/lists/50c6d144-ae86-11eb-a3d0-06b4694bee2a/contacts',
         {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(body),
+          method: 'post',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          mode: 'no-cors',
         },
       ).then((response) => response.json());
-    },
-  });
 
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
+      alert(JSON.stringify(data, null, 2));
+      alert(JSON.stringify(response, null, 2));
+    }}
+  >
+    {({ isSubmitting }) => (
+      <Form>
+        <label htmlFor="email">Email Address</label>
+        <Field id="email" name="email" type="email" />
 
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
-
+        <button type="submit" disabled={isSubmitting}>
+          Submit
+        </button>
+      </Form>
+    )}
+  </Formik>
+);
 export default FooterForm;

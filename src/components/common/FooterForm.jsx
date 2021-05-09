7f475ff2ebe2a29ead3fc0b6882689c4c2701cd7
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2021-05-09 09:42:18 +0800

    Change FooterForm to Airtable

--- src/components/common/FooterForm.jsx
+++ src/components/common/FooterForm.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import { Formik, Field, Form } from 'formik';
-import fetch from 'node-fetch';
+import Airtable from 'airtable';
+import { PageMetadata } from '@/components/common/TextStyles';
+import { Button } from '@/components/Button';
 
+const base = new Airtable({ apiKey: 'keyfVdncmxXanjijI' }).base(
+  'appEQtq8ScqFUGgrL',
+);
+
+function validateEmail(value) {
+  let error;
+
+  if (!value) {
+    error = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    error = 'Invalid email address';
+  }
+
+  return error;
+}
 const FooterForm = () => (
   // Pass the useFormik() hook initial form values and a submit function that will
 
@@ -31,37 +48,53 @@ const FooterForm = () => (
   <Formik
     initialValues={{
       email: '',
+      name: '',
     }}
     onSubmit={async (values) => {
-      const data = {
-        api_key: '69dbb0df-2884-4aa8-aa8b-801c483963dc',
-        email_address: values.email,
-      };
-
-      fetch(
-        'https://emailoctopus.com/api/1.5/lists/50c6d144-ae86-11eb-a3d0-06b4694bee2a/contacts',
-        {
-          method: 'post',
-          body: JSON.stringify(data),
-          headers: {
-            'Content-Type': 'application/json',
+      base('Subscriber').create(
+        [
+          {
+            fields: {
+              Email: values.email,
+              Name: values.name,
+            },
           },
-          mode: 'no-cors',
+        ],
+        (err) => {
+          if (err) {
+            console.error(err);
+            console.log(values);
+          }
         },
-      ).then((response) => response.json());
+      );
 
-      alert(JSON.stringify(data, null, 2));
-      alert(JSON.stringify(response, null, 2));
+      // fetch(
+      //   'https://emailoctopus.com/api/1.5/lists/50c6d144-ae86-11eb-a3d0-06b4694bee2a/contacts',
+      //   {
+      //     method: 'post',
+      //     body: JSON.stringify(data),
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //     },
+      //     mode: 'no-cors',
+      //   },
+      // ).then((response) => response.json());
     }}
   >
-    {({ isSubmitting }) => (
+    {({ errors, touched, isValidating, isSubmitting }) => (
       <Form>
-        <label htmlFor="email">Email Address</label>
-        <Field id="email" name="email" type="email" />
-
-        <button type="submit" disabled={isSubmitting}>
+        <PageMetadata css={{ color: '$green100' }} type="label" htmlFor="email">
+          Email Address
+        </PageMetadata>
+        <Field validate={validateEmail} id="email" name="email" type="email" />{' '}
+        {errors.email && touched.email && <span>{errors.email}</span>}
+        <PageMetadata css={{ color: '$green100' }} type="label" htmlFor="name">
+          Name
+        </PageMetadata>
+        <Field id="name" name="name" type="Name" />
+        <Button color="onPrimary" type="submit" disabled={isSubmitting}>
           Submit
-        </button>
+        </Button>
       </Form>
     )}
   </Formik>

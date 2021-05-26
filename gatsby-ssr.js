83cc4876d2b28bae4d15bd399d3a70c30c6ebad3
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2021-05-26 19:00:35 +0800

    Fix: Meta Tag SSR Fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,23 @@
 import * as React from 'react';
+import { Helmet } from 'react-helmet';
 import { getCssString } from './src/gatsby-theme-stitches/stitches.config.ts';
 
-export const onRenderBody = ({ setHeadComponents }) => {
+export const onRenderBody = ({
+  setHeadComponents,
+  setHtmlAttributes,
+  setBodyAttributes,
+}) => {
+  const helmet = Helmet.renderStatic();
+  setHtmlAttributes(helmet.htmlAttributes.toComponent());
+  setBodyAttributes(helmet.bodyAttributes.toComponent());
+  setHeadComponents([
+    helmet.title.toComponent(),
+    helmet.link.toComponent(),
+    helmet.meta.toComponent(),
+    helmet.noscript.toComponent(),
+    helmet.script.toComponent(),
+    helmet.style.toComponent(),
+  ]);
   setHeadComponents([
     <style
       id="stitches"
@@ -10,4 +26,22 @@ export const onRenderBody = ({ setHeadComponents }) => {
       }}
     />,
   ]);
+};
+
+export const onPreRenderHTML = ({
+  getHeadComponents,
+  replaceHeadComponents,
+}) => {
+  const headComponents = getHeadComponents();
+
+  headComponents.sort((x, y) => {
+    if (x.props && x.props['data-react-helmet']) {
+      return -1;
+    } else if (y.props && y.props['data-react-helmet']) {
+      return 1;
+    }
+    return 0;
+  });
+
+  replaceHeadComponents(headComponents);
 };
